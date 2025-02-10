1b6a789ed4bf3ca613e37a962d86530a4e8e3c8a
Ricardo <rimatiga04@gmail.com>
2025-02-10 10:57:48 +0100

    Cambiamos los archivos para publicarlos en github pages

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,22 @@
 // calculadora.js
 
-function sumar(a, b) {
+function suma(a, b) {
     return a + b;
 }
-
-function restar(a, b) {
+function resta(a, b) {
     return a - b;
 }
-
-
-module.exports = { sumar, restar };
+function multiplicar(a, b) {
+    return a * b;
+}
+function dividir(a, b) {
+    if (b === 0) {
+        throw new Error("No se puede dividir por cero");
+    }
+    return a / b;
+}
+// Hacer las funciones accesibles en el navegador
+window.suma = suma;
+window.resta = resta;
+window.multiplicar = multiplicar;
+window.dividir = dividir;
